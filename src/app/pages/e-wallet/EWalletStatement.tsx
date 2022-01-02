c598b52756fe8bf95ed1a63180366abee9fbf7f0
Adedolapo John Olaiya <ajolaiya@sidmach.com>
2022-01-02 08:33:11 +0100

    Refactored userearnings and Ewallet statement pages

--- src/app/pages/e-wallet/EWalletStatement.tsx
+++ src/app/pages/e-wallet/EWalletStatement.tsx
@@ -2,15 +2,39 @@
 import React, { FC, useEffect } from 'react'
 import { useIntl } from 'react-intl'
 import { PageTitle } from '../../../_metronic/layout/core'
+import MaterialTable, { Column } from "@material-table/core";
+import tableIcons from "../../constants/TableIcons";
 const faker = require('faker');
 
 
 const E_WalletStatement: FC = () => {
+
+  interface IE_WalletStatement {
+    description: string;
+    amount: number;
+    transactionDate: string;
+    balance: number;
+  }
   const [transaction, setTransaction] = React.useState([])
+  const [totalBalance, setTotalBalance] = React.useState(0)
+
+  const data: Array<IE_WalletStatement> = [...transaction];
+
+  function formatPrice(Symbol: any) {
+    return function(value: any) {
+       const price = (value);
+       return `${Symbol} ${price}`;
+    };
+ }
+ 
+  const PriceLabel = formatPrice('£');
+
+
+
   useEffect(() => {
     let datalength = 30
     let sn = 1;
-    const userEarnings = [... Array(datalength)] 
+    const ewalletStatement = [... Array(datalength)] 
                      .map(_ => ({ 
                          sn: sn++,
                          description: faker.random.words(),
@@ -18,76 +42,77 @@ const E_WalletStatement: FC = () => {
                        transactionDate: new Date(faker.date.recent()).toDateString(),
                          balance: faker.commerce.price(),
                      }));
-    setTransaction(userEarnings) 
+                     const result = ewalletStatement.reduce((amount, sum) => {
+                      // return the sum with previous value
+                      return parseFloat(amount) + parseFloat(sum.balance);
+                      // set initial value as 0
+                    }, 0);
+    setTransaction(ewalletStatement) 
+    setTotalBalance(result)
   
   }, [setTransaction])
 
   return (
     <>
-    <div className='card'>
-   
-      {/* begin::Body */}
-      <div className='card-body py-3'>
-           {/* begin::Table container */}
-           <div className='table-responsive'>
-            {/* begin::Table */}
-            <table className='table table-rounded table-striped border align-middle gs-7 gy-7'>
-              {/* begin::Table head */}
-              <thead>
-                <tr className='fw-bolder text-gray-800 border-bottom border-gray-200 bg-light'>
-                  <th className='ps-4 min-w-100px rounded-start'>S/N</th>
-                  <th className='min-w-300px'>Description</th>
-                  <th className='min-w-125px'>Amount</th>
-                  <th className='min-w-250px'>Transaction Date</th>
-                  <th className='min-w-125px rounded-end'>Balance</th>
-                </tr>
-              </thead>
-              {/* end::Table head */}
-              {/* begin::Table body */}
-              <tbody>
-                {transaction.map((trans, i) => (
-                  <tr key={i}>
-                    <td><div className='d-flex align-items-center'>
-                   
-                      <div className='d-flex justify-content-start flex-column'>
-                        {trans.sn}
-                      </div>
-                    </div></td>
-                    <td>
-                    <a href='#' className='text-dark fw-bolder text-hover-primary d-block mb-1 fs-6'>
-                        {trans.description}
-                    </a>
-                  </td>
-                  <td>
-                    <a href='#' className='text-dark fw-bolder text-hover-primary d-block mb-1 fs-6'>
-                        {`+$` + trans.amount}
-                    </a>
-                    </td>
-                    <td>
-                    <a href='#' className='text-dark fw-bolder text-hover-primary d-block mb-1 fs-6'>
-                        {trans.transactionDate}
-                    </a>
-                    </td>
-                    <td>
-                    <a href='#' className='text-dark fw-bolder text-hover-primary d-block mb-1 fs-6'>
-                        {`$` + trans.balance}
-                    </a>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-              {/* end::Table body */}
-            </table>
-            {/* end::Table */}
-          </div>
-          {/* end::Table container */}  
+      <div className='mt-1'>
+      <div className='card'>
+            <div className='card-body py-2 d-flex justify-content-end'>
+              <span className='fw-bold'>E-Wallet Balance:  <span className='text-primary'>{PriceLabel(totalBalance)}</span></span>
+            </div>
+        </div>
+        <div className='mt-3'>
+          {/* begin::Table container */}
+          <MaterialTable<RowInterface>
+            icons={tableIcons}
+            title=""
+            style={{ whiteSpace: "nowrap" }}
+            columns={[
+              {
+                title: 'S/N',
+                render: (rowData) => rowData.tableData.id + 1,
+              },
+              {
+                title: 'Description',
+                field: 'description'
+              },
+              {
+                title: 'Amount',
+                render: (rowData) => {
+                  return <p style={{ color: "#2B9E46" }}> {'+' + ' ' + PriceLabel(rowData.amount)}</p>
+                },
+              },
+              {
+                title: 'Transaction Date',
+                field: 'transactionDate'
+              },
+              {
+                title: 'Balance',
+                render: (rowData) =>
+                    PriceLabel(rowData.balance),  
+                }
+
+            ]}
+            data={data}
+            // actions={[
+            // ]}
+            options={{
+              actionsColumnIndex: -1,
+              exportButton: true,
+              filtering: false,
+              padding: 'dense',
+              pageSize: 10,
+              headerStyle: {
+                fontSize: '13px',
+                backgroundColor: '#ccc',
+                borderBottomColor: '#111',
+                paddingTop: '5px',
+                paddingBottom: '5px',
+              },
+            }}
+          />
+          {/* end::Table container */}
+        </div>
       </div>
-      {/* begin::Body */}
-  
-  
-    
-    
-    </div>
   </>
   )
 }
--- src/app/pages/user-earnings/UserEarning.tsx
+++ src/app/pages/user-earnings/UserEarning.tsx
@@ -2,94 +2,173 @@
 import React, { FC, useEffect } from 'react'
 import { useIntl } from 'react-intl'
 import { PageTitle } from '../../../_metronic/layout/core'
+import MaterialTable, { Column } from "@material-table/core";
+import tableIcons from "../../constants/TableIcons";
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Button from "@material-ui/core/Button";
+
 const faker = require('faker');
 
 const UserEarning = () => {
+  interface IEarning {
+    category: string;
+    amount: number;
+    transactionDate: string;
+  }
+
   const [transaction, setTransaction] = React.useState([])
-  
+  const [category, setCategory] = React.useState(0)
+  const [daterange, setDaterange] = React.useState(0)
+  const [totalAmount, setTotalAmount] = React.useState(0)
+
+  const data: Array<IEarning> = [...transaction];
+
+  const columns: Array<Column<IEarning>> = [
+    { title: "Category", field: "category" },
+    { title: "Amount", field: "amount", type: 'numeric' },
+    { title: "Transaction Date", field: "transactionDate" },
+  ];
+
+  function formatPrice(Symbol: any) {
+    return function(value: any) {
+       const price = (value);
+       return `${Symbol} ${price}`;
+    };
+ }
+ 
+  const PriceLabel = formatPrice('£');
+
   useEffect(() => {
     let datalength = 30
     let sn = 1;
-    const userEarnings = [... Array(datalength)] 
-                     .map(_ => ({ 
-                         sn: sn++,
-                         category: faker.commerce.department(),
-                         amount: faker.commerce.price(),
-                         transactionDate: new Date(faker.date.recent()).toDateString(),
-                     }));
-    setTransaction(userEarnings) 
-  
+    const userEarnings = [...Array(datalength)]
+      .map(_ => ({
+        sn: sn++,
+        category: faker.commerce.department(),
+        amount: faker.commerce.price(),
+        transactionDate: new Date(faker.date.recent()).toDateString(),
+      }));
+      const result = userEarnings.reduce((amount, sum) => {
+        // return the sum with previous value
+        return parseFloat(amount) + parseFloat(sum.amount);
+        // set initial value as 0
+      }, 0);
+    setTransaction(userEarnings)
+    setTotalAmount(result)
+
   }, [setTransaction])
-  console.log(transaction)
-  
+
+
   return (
     <>
       <div className='card'>
-
-        {/* begin::Header */}
-        <div className='card-header border-0 pt-5'>
-          <h3 className='card-title align-items-start flex-column'>
-            {/* <span className='card-label fw-bolder fs-3 mb-1'>User Earnings</span> */}
-            {/* <span className='text-muted mt-1 fw-bold fs-7'>Over 500 new members</span> */}
-          </h3>
-        </div>
-        {/* end::Header */}
-        {/* begin::Body */}
-        <div className='card-body py-3'>
-          {/* begin::Table container */}
-          <div className='table-responsive'>
-            {/* begin::Table */}
-            <table className='table table-rounded table-striped border align-middle gs-7 gy-7'>
-              {/* begin::Table head */}
-              <thead>
-                <tr className='fw-bolder text-gray-800 border-bottom border-gray-200 bg-light'>
-                  <th className='ps-4 min-w-100px rounded-start'>S/N</th>
-                  <th className='min-w-200px'>Category</th>
-                  <th className='min-w-125px'>Amount</th>
-                  <th className='min-w-200px'>Transaction Date</th>
-                  {/* <th className='min-w-200px text-end rounded-end'></th> */}
-                </tr>
-              </thead>
-              {/* end::Table head */}
-              {/* begin::Table body */}
-              <tbody>
-                {transaction.map((trans, i) => (
-                  <tr key={i}>
-                    <td><div className='d-flex align-items-center'>
-                   
-                      <div className='d-flex justify-content-start flex-column'>
-                        {trans.sn}
-                      </div>
-                    </div></td>
-                    <td>
-                    <a href='#' className='text-dark fw-bolder text-hover-primary d-block mb-1 fs-6'>
-                        {trans.category}
-                    </a>
-                  </td>
-                  <td>
-                    <a href='#' className='text-dark fw-bolder text-hover-primary d-block mb-1 fs-6'>
-                        {`$` + trans.amount}
-                    </a>
-                    </td>
-                    <td>
-                    <a href='#' className='text-dark fw-bolder text-hover-primary d-block mb-1 fs-6'>
-                        {trans.transactionDate}
-                    </a>
-                  </td>
-                  </tr>
-                ))}
-              </tbody>
-              {/* end::Table body */}
-            </table>
-            {/* end::Table */}
+        <div className='card-body row py-3'>
+          <div className="col-md-3 mt-2 ">
+            <FormControl className="w-100 mb-2">
+              <InputLabel htmlFor="age-simple">
+                Category
+              </InputLabel>
+              <Select
+                value={category}
+                onChange={(event) => {
+                  setCategory(event.target.value)
+                }}
+              >
+                <MenuItem value={0}>
+                  Any
+                </MenuItem>
+                <MenuItem value={1}>
+                  Referral Commission
+                </MenuItem>
+                <MenuItem value={2}>
+                  Rank Commission
+                </MenuItem>
+                <MenuItem value={3}>
+                  Level Commission
+                </MenuItem>
+                <MenuItem value={4}>
+                  Binary Commission
+                </MenuItem>
+              </Select>
+            </FormControl>
           </div>
+          <div className="col-md-3 mt-2">
+            <FormControl className="w-100 mb-2">
+              <InputLabel htmlFor="age-simple">
+                Date Range
+              </InputLabel>
+              <Select
+                value={daterange}
+                onChange={(event) => {
+                  setDaterange(event.target.value)
+                }}
+              >
+                <MenuItem value={0}>
+                  Overall
+                </MenuItem>
+                <MenuItem value={1}>
+                  Today
+                </MenuItem>
+                <MenuItem value={2}>
+                  This Month
+                </MenuItem>
+                <MenuItem value={3}>
+                  This Year
+                </MenuItem>
+                <MenuItem value={4}>
+                  Custom
+                </MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <div className='col-md-6 mt-2'>
+            <Button className='btn btn-sm btn-primary mx-2'>Search</Button>
+            <Button className='btn btn-sm btn-secondary mx-2'>Clear</Button>
+          </div>
+
+        </div>
+      </div>
+      <div className='mt-4'>
+          <div className='card'>
+            <div className='card-body py-2 d-flex justify-content-end'>
+              <span className='fw-bold'>Total Commission Earned:  {PriceLabel(totalAmount)}</span>
+            </div>
+        </div>
+        <div className='mt-3'>
+          {/* begin::Table container */}
+          <MaterialTable<RowInterface>
+            icons={tableIcons}
+            title=""
+            style={{ whiteSpace: "nowrap" }}
+            columns={[
+              {
+                title: 'S/N',
+                render: (rowData) => rowData.tableData.id + 1,
+              }, ...columns
+            ]}
+            data={data}
+            // actions={[
+            // ]}
+            options={{
+              actionsColumnIndex: -1,
+              exportButton: true,
+              filtering: false,
+              padding: 'dense',
+              pageSize: 10,
+              headerStyle: {
+                fontSize: '13px',
+                backgroundColor: '#ccc',
+                borderBottomColor: '#111',
+                paddingTop: '5px',
+                paddingBottom: '5px',
+              },
+            }}
+          />
           {/* end::Table container */}
         </div>
-        {/* begin::Body */}
-  
-  
-    
-    
       </div>
     </>
   )
