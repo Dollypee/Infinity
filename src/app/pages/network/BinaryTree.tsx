c04b0a1b738cdc7c843c16c7a9aab403ca6b0033
AY-BAMZ <aybamz181@gmail.com>
2022-01-02 23:31:31 +0100

    tree init

--- src/app/pages/network/BinaryTree.tsx
+++ src/app/pages/network/BinaryTree.tsx
@@ -4,6 +4,7 @@
 import React, { FC } from 'react'
 import { useIntl } from 'react-intl'
 import { PageTitle } from '../../../_metronic/layout/core'
+import { Tree, TreeNode } from 'react-organizational-chart';
 import {
     MixedWidget2,
     MixedWidget10,
@@ -19,6 +20,37 @@ import {
     MixedWidget12,
     TablesWidget13
   } from '../../../_metronic/partials/widgets'
+
+  const StyledNode = styled.div`
+  padding: 5px;
+  border-radius: 8px;
+  display: inline-block;
+  border: 1px solid red;
+`;
+
+
+const StyledTreeExample = () => (
+  <Tree
+    lineWidth={'2px'}
+    lineColor={'green'}
+    lineBorderRadius={'10px'}
+    label={<StyledNode>Root</StyledNode>}
+  >
+    <TreeNode label={<StyledNode>Child 1</StyledNode>}>
+      <TreeNode label={<StyledNode>Grand Child</StyledNode>} />
+    </TreeNode>
+    <TreeNode label={<StyledNode>Child 2</StyledNode>}>
+      <TreeNode label={<StyledNode>Grand Child</StyledNode>}>
+        <TreeNode label={<StyledNode>Great Grand Child 1</StyledNode>} />
+        <TreeNode label={<StyledNode>Great Grand Child 2</StyledNode>} />
+      </TreeNode>
+    </TreeNode>
+    <TreeNode label={<StyledNode>Child 3</StyledNode>}>
+      <TreeNode label={<StyledNode>Grand Child 1</StyledNode>} />
+      <TreeNode label={<StyledNode>Grand Child 2</StyledNode>} />
+    </TreeNode>
+  </Tree>
+);
 
 const Binary_Tree_Page: FC = () => (
   <>
@@ -47,6 +79,7 @@ const BinaryTreePage: FC = () => {
   return (
     <>
       <PageTitle breadcrumbs={[]}>{intl.formatMessage({ id: 'Referral Network' })}</PageTitle>
+      <StyledTreeExample />
       <Binary_Tree_Page />
     </>
   )
